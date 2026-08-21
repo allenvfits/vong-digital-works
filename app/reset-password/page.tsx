0deb98fc71bf89c34893192257f77e39876402aa
Allen <Hagotchaaa@gmail.com>
2026-08-21 05:23:10 -0700

    Add secure password recovery page

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,3 @@
+import ResetPasswordForm from "./ResetPasswordForm";
+export const metadata={title:"Reset Password | Vong Digital Works",robots:{index:false,follow:false},openGraph:{images:[]},twitter:{images:[]}};
+export default function ResetPasswordPage(){return <main className="loginPage"><ResetPasswordForm/></main>}
