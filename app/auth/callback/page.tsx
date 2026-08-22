133f12939d3f3ca57e5c247c4e1476e0ad039488
allenvfits <112698116+allenvfits@users.noreply.github.com>
2026-08-21 18:04:13 -0700

    Add Google and Apple portal sign-in

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,7 @@
+import OAuthCallback from "./OAuthCallback";
+
+export const metadata = { title: "Completing Sign In | Vong Digital Works", robots: { index: false, follow: false } };
+
+export default function AuthCallbackPage() {
+  return <OAuthCallback />;
+}
